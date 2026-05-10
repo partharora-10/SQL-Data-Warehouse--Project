ec06397dd6dd599f4585c30bfe5840497b5fc1b6
Parth Arora <partharorasml@gmail.com>
2026-05-10 15:11:04 +0530

    Create init_database.sql

---
+++ scrips/init_database.sql
@@ -0,0 +1,12 @@
+We are creating a Database having name "Datawarehouse".
+  This database will have the schemas in it to define their working level - Bronze , Silver , Gold.
+
+Create Database Datawarehouse;
+Go
+Use Datawarehouse;
+Go
+Create Schema Bronze; 
+Go 
+Create Schema Silver;
+Go
+Create Schema Gold;
